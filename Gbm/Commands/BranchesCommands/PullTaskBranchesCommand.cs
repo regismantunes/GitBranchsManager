4a6c2ccc98095671370a4817d3467400ea58b3f8
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2025-11-03 09:18:18 -0400

    Created new task to Pull branches

--- Gbm/Commands/BranchesCommands/PullTaskBranchesCommand.cs
+++ Gbm/Commands/BranchesCommands/PullTaskBranchesCommand.cs
@@ -1,19 +1,19 @@
-using Gbm.Services.Git;
+﻿using Gbm.Services.Git;
 using RA.Console.DependencyInjection.Attributes;
 
 namespace Gbm.Commands.BranchesCommands
 {
-    public class PushTaskBranchesCommand(IGitTool gitTool)
+    public class PullTaskBranchesCommand(IGitTool gitTool)
     {
-        [CommandAsyncWithArgsBuilderAsync<BranchesCommandArgsBuilder>("-push",
-            Description = "Push task branches",
-            Example = "gbm -push <TaskId> [Repos...]",
+        [CommandAsyncWithArgsBuilderAsync<BranchesCommandArgsBuilder>("-pull",
+            Description = "Pull task branches",
+            Example = "gbm -pull <TaskId> [Repos...]",
             Group = CommandGroups.Branches,
-            Order = 4)]
+            Order = 5)]
         public async Task<int> ExecuteAsync(string taskBranch, string[] repositories, CancellationToken cancellationToken = default)
         {
             gitTool.ShowGitOutput = true;
-            MyConsole.WriteCommandHeader("📤 Pushing task branches...");
+            MyConsole.WriteCommandHeader("📥 Pulling task branches...");
             foreach (var repo in repositories)
             {
                 MyConsole.WriteHeader($"--- Processing repository: {repo} ---");
@@ -21,12 +21,12 @@ namespace Gbm.Commands.BranchesCommands
 
                 MyConsole.WriteStep($"→ Checking out to '{taskBranch}'");
                 await gitTool.CheckoutAsync(taskBranch, cancellationToken);
-                
-                MyConsole.WriteStep($"→ Pushing '{taskBranch}'");
-                await gitTool.PushAsync(cancellationToken);
+
+                MyConsole.WriteStep($"→ Pulling '{taskBranch}'");
+                await gitTool.PullAsync(cancellationToken);
             }
 
-            MyConsole.WriteSucess("✅ Branch's task pushed!");
+            MyConsole.WriteSucess("✅ Branch's task pulled!");
             return 0;
         }
     }
