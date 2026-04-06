7b4e9a26af8811d11c21e80c4684b803534d0d32
Regis Antunes <65835931+regismantunes@users.noreply.github.com>
2026-04-06 13:24:11 -0400

    Created GetVersion command

---
+++ Gbm/Commands/ConfigurationCommands/GetVersionCommand.cs
@@ -0,0 +1,19 @@
+﻿using RA.Console.DependencyInjection.Attributes;
+
+namespace Gbm.Commands.ConfigurationCommands
+{
+    public class GetVersionCommand
+    {
+        [Command("-v",
+            Description = "Get Gbm version",
+            Example = "gbm -v",
+            Group = CommandGroups.Configuration,
+            Order = 1)]
+        public int Execute()
+        {
+            var version = GetType().Assembly.GetName().Version?.ToString() ?? "Unknown";
+            MyConsole.WriteInfo($"Gbm version: {version}");
+            return 0;
+        }
+    }
+}
